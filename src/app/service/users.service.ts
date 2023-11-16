1508bb1e7e16adb4905d21b83eba459818252a45
Marine <mariine.becker@gmail.com>
2023-11-16 11:39:46 +0100

    debut td2

---
+++ src/app/service/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {UserLdap} from "../models/user-ldap";
+import {LDAP_USERS} from "../models/ldap-mock-data";
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+// Liste des utilisateurs
+  users: UserLdap[] = LDAP_USERS;
+
+  getUsers(): Observable<UserLdap[]> {
+    return of(this.users);
+  }
+  constructor() { }
+}
